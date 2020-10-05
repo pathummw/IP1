2e3851de415740b4970325ae981f71700cec8b2f
Pathum <pathummadushanka.weerathunga@student.kyh.se>
2020-10-05 18:16:07 +0200

    Navbar and hamburger menu working without bugs

--- hamburger.js
+++ hamburger.js
@@ -6,66 +6,23 @@ let img = document.querySelector("img");
 let firstClick = true;  /* Set the boolean value when hanburger button clicked to change the state of the burger button */
 
 
-/* window.addEventListener('resize', setEqualHeight);  */ /* TEST */
-/* const mediaQuery = window.matchMedia('(min-width: 800px)') */
-
-function setEqualHeight(e) {
-    /* let viewportWidth = parseInt(document.documentElement.clientWidth);
-    if(viewportWidth > 750){
-        document.querySelector("nav").style.visibility = "visible";
-        console.log("NAV VISIBLE");
-    } */
-}
-
 window.addEventListener("resize", reSizeFunction);
 
 function reSizeFunction(){
-    if(parseInt(document.documentElement.clientWidth) >= 810){
-        burgerBtn.style.visibility = "hidden";
-        document.querySelector("nav").style.visibility = "visible";
+
+    /* check whether the window is resized and if the windows is resized more than 750px, set nav bar visible */
+    if(parseInt(document.documentElement.clientWidth) >= 750){
+        if(document.querySelector("nav").style.visibility == "hidden"){
+            document.querySelector("nav").style.visibility = "visible"; 
+        }
     }
-    else{
+    else if(parseInt(document.documentElement.clientWidth) < 750 && firstClick == true){
         document.querySelector("nav").style.visibility = "hidden";
-        burgerBtn.style.visibility = "visible";
     }
-    /* console.log("Resized: "+window.innerWidth+' Client Width: '+document.documentElement.clientWidth); */
 }
 
 
-
 burgerBtn.addEventListener("click",function(event){
-    /* burgerBtn.style.transition = '2s'; */
-    console.log("Func call");
-    
-    /* if(1==1){ */
-        /* document.querySelector("span").style.backgroundColor = "red";
-        document.querySelector("span").style.transform = "rotate(135deg)"; */
-        /* spanElements[0].style.transform = "rotate(45deg)";
-        spanElements[0].style.width = "30px"; */
-        /* spanElements[1].style.backgroundColor = "red"; */
-        /* spanElements[2].style.transform = "rotate(-45deg)";
-        spanElements[2].style.width = "30px"; */
-
-        /* transform: rotate(45deg);
-        transform: rotate(-45deg); */
-
-        /* navBar.style.visibility = "visible";
-
-        listButtons.forEach(element => element.style.width = "80vw"); */
-        /* listButtons[4].style.width = "80vw"; */
-        /* console.log(listButtons); */
-        /* clicked = true; */
-        
-    /* } */
-    /*if(1==1){  //set hamburger button to normal stage
-        /* spanElements[0].style.transform = "rotate(0deg)";
-        spanElements[0].style.width = "10px";
-        /* spanElements[1].style.backgroundColor = "red"; 
-        spanElements[2].style.transform = "rotate(0deg)";
-        spanElements[2].style.width = "10px";
-        clicked = false; */
-        /* console.log("clicked second"); 
-    } */
 
     if(firstClick){
         spanElements[1].style.backgroundColor = "red";
@@ -73,11 +30,9 @@ burgerBtn.addEventListener("click",function(event){
         spanElements[0].style.transform = "rotate(45deg) translate(9px, 8px)";
         spanElements[1].style.opacity = "0";
         spanElements[2].style.transform = "rotate(-45deg) translate(7px, -5px)"; 
-        /* navBar.style.visibility = "visible"; */
         document.querySelector("nav").style.zIndex = "";
         burgerBtn.style.zIndex = "1";
         document.querySelector("nav").style.visibility = "visible";
-
         firstClick = false;
     }
     else if(!firstClick){
@@ -85,19 +40,11 @@ burgerBtn.addEventListener("click",function(event){
         spanElements[0].style.transform = "rotate(0deg)";
         spanElements[1].style.opacity = "100";
         spanElements[2].style.transform = "rotate(0deg)";
-        document.querySelector("nav").style.zIndex = "2";
-        burgerBtn.style.zIndex = "1";
+        document.querySelector("nav").style.zIndex = "1";
+        burgerBtn.style.zIndex = "2";
         document.querySelector("nav").style.visibility = "hidden";
-
         firstClick = true;
     }
-
-    
-
-
-
-
-
 
 
 }); 
